bb1a098d4cb2840609194f18ea6d05dc4706aa23
matthieu rougeolle <matthieu.rougeolle@gmail.com>
2016-12-14 19:54:40 +0100

    +

--- public/angular-app/main/zoosFactory.js
+++ public/angular-app/main/zoosFactory.js
@@ -2,31 +2,31 @@ angular.module("zooApp").factory("zoosFactory", zoosFactory);
 
 function usersFactory($http) {
 	return {
-		userGetAll : userGetAll,
-		userAddOne : userAddOne,
-		userGetOne : userGetOne,
-        userDelete : userDelete,
-        userUpdate : userUpdate
+		zooGetAll : zooGetAll,
+		zooAddOne : zooAddOne,
+		zooGetOne : zooGetOne,
+        zooDelete : zooDelete,
+        zooUpdate : zooUpdate
 	};
 
-function userGetAll() {
-	return $http.get("/api/users").then(complete).catch(error);
+function zooGetAll() {
+	return $http.get("/api/zoos").then(complete).catch(error);
 }
 
-function userAddOne(postData) {
-	return $http.post("/api/users/add", postData).then(complete).catch(error);
+function zooAddOne(postData) {
+	return $http.post("/api/zoos/add", postData).then(complete).catch(error);
 }
 
-function userDelete(id) {
-	return $http.delete('/api/users/' +id).then(complete).catch(error);
+function zooDelete(id) {
+	return $http.delete('/api/zoos/' +id).then(complete).catch(error);
 }
 
-function userGetOne(id) {
-	return $http.get('/api/users/' +id).then(complete).catch(error);
+function zooGetOne(id) {
+	return $http.get('/api/zoos/' +id).then(complete).catch(error);
 }
 
-function userUpdate(id, user) {
-	return $http.put('/api/users/update/' +id, user).then(complete).catch(error);
+function zooUpdate(id, zoo) {
+	return $http.put('/api/zoos/update/' +id, zoo).then(complete).catch(error);
 }
 
 function complete(response) {
